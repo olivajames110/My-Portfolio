9a455f12a790bcbbe3e4bb862f5e364ddd3ac826
olivajames110 <olivajames110@gmail.com>
2018-05-22 10:48:24 -0400

    Mobile Toggle
    Add mobile functionality

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -3,20 +3,41 @@ import ReactDOM from 'react-dom';
 import './header.css'
 
 class Header extends React.Component {
-  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isMenuOpen: false
+    };
+  }
+
+  handleMenuToggleOnClick = (e) => {
+    this.setState({ isMenuOpen: !this.state.isMenuOpen });
+  }
   
  render() {
+  const isMenuOpen = this.state.isMenuOpen ? 'initial' : 'none';
+
   return (
     <nav className="">
       <div className="toggle">
         <div className="mobile-logo">Jimmy Oliva</div>
-        <div className="mobile-nav"><i class="fas fa-bars" aria-hidden="true"></i></div>
+        <div className="mobile-nav">
+          <i
+            className="fas fa-bars"
+            aria-hidden="true"
+            onClick={this.handleMenuToggleOnClick}
+          />
+        </div>
       </div>
       <div className="desktop-header fluid-container">
         <div className="row">
         <div className="logo">Jimmy Oliva</div>
         <div className="navigation">
-          <ul className="nav-links">
+          <ul
+            className="nav-links"
+            style={{ display: isMenuOpen }}
+          >
           <li><a href="#"><span>Home</span></a></li>
           <li><a href="#"><span>About</span></a></li>
           <li><a href="#"><span>Portfolio & Projects</span></a></li>
