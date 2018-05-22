13fe0775ee9c4474c84d93648f5ffb46abaf0c44
olivajames110 <olivajames110@gmail.com>
2018-05-22 09:00:41 -0400

    Added Mobile Header
    Along with bug fixes

--- src/components/AboutSection/aboutSection.js
+++ src/components/AboutSection/aboutSection.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './aboutSection.css';
 
 class aboutSection extends React.Component {
  render() {
   return (
-   <div>Boasddy</div>
+   <div className="about-container container">Boasddy</div>
   )
  }
 }
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -3,21 +3,29 @@ import ReactDOM from 'react-dom';
 import './header.css'
 
 class Header extends React.Component {
+  
+  
  render() {
   return (
-    <div className="header fluid-container">
-      <div className="row">
-       <div className="logo">Jimmy Oliva</div>
-       <div className="navigation">
-        <ul className="nav-links">
-         <li><a href="#"><span>Home</span></a></li>
-         <li><a href="#"><span>About</span></a></li>
-         <li><a href="#"><span>Portfolio & Projects</span></a></li>
-         <li><a href="#"><span>Contact</span></a></li>
-        </ul>
-       </div>
+    <nav className="">
+      <div className="toggle">
+        <div className="mobile-logo">Jimmy Oliva</div>
+        <div className="mobile-nav"><i class="fas fa-bars" aria-hidden="true"></i></div>
       </div>
-    </div>
+      <div className="desktop-header fluid-container">
+        <div className="row">
+        <div className="logo">Jimmy Oliva</div>
+        <div className="navigation">
+          <ul className="nav-links">
+          <li><a href="#"><span>Home</span></a></li>
+          <li><a href="#"><span>About</span></a></li>
+          <li><a href="#"><span>Portfolio & Projects</span></a></li>
+          <li><a href="#"><span>Contact</span></a></li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    </nav>
   );
 }
 }
