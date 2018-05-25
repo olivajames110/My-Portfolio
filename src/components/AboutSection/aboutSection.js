3e5965e3ba5dd1dbf67cbaeb3e00d29ffe30cd33
olivajames110 <olivajames110@gmail.com>
2018-05-25 11:11:47 -0400

    Added Buttons
    Added social media buttons for about us section

--- src/components/AboutSection/aboutSection.js
+++ src/components/AboutSection/aboutSection.js
@@ -12,8 +12,16 @@ class aboutSection extends React.Component {
      </div>
      <div className="col-md-1"></div>
      <div className="col-md-7">
-      <h1 className="about-text-title">My Abilities</h1>
+      <h1 className="about-text-title">A Little About Myself</h1>
       <div className="about-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lacus nisl, euismod non nulla quis, pretium pellentesque est. In dictum libero diam, ut scelerisque neque pharetra ut. Nam eu ornare mauris. Donec sapien risus, finibus ac turpis et, elementum feugiat orci. Curabitur cursus tortor sit amet arcu dignissim pharetra. Fusce sodales consequat interdum. Suspendisse potenti. Curabitur lacinia porttitor massa, quis mollis eros lobortis nec. Nulla congue sem sed sem pellentesque, a rhoncus dui ultricies. Proin ultricies, metus quis dapibus ultricies, mauris neque porta ex, ut bibendum massa lectus vitae quam.
+      </div>
+      <div className="about-sm-resume-container">
+       <button className="about-button" >
+        <i class="far fa-envelope-open"></i><span>Send me a message </span>
+       </button>
+       <button className="about-button" >
+        <i class="fab fa-linkedin-in"></i>Lets link up
+        </button>
       </div>
      </div>
     </div>
